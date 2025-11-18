6399738f4c308dd4a0d35f974930b14d5d9fd68e
YAHYA <yahya.abdul.s87@Kalvium.community>
2025-11-18 09:48:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -3,10 +3,10 @@
 A React app showcasing my favourite movies built with Vite
 
 ## Links
-- **GitHub Repository:** [https://github.com/waseem-pp/movie-showcase.git]
+- **GitHub Repository:** [https://github.com/yahyakalvium/movie_showcase.git]
 - **Live Website:** [https://movieshowcasewaseem.netlify.app/]
 
 ## Run Locally
-1. Clone the repository
+1. Clone the repository by using `git clone`
 2. Run `npm install`
 3. Run `npm run dev`
--- src/data/movies.js
+++ src/data/movies.js
@@ -4,27 +4,23 @@ export const movies= [
         title: 'Journey to the Centre of the Earth',
         genre: 'Action, Adventure',
         why: 'Thriller adventure movie and exploring the other side of the earth where many unpredicted things happens.',
-        image: '/JTCOE.jpeg'
     },
     {
         id: 2,
         title: 'Inception',
         genre: 'Sci-Fi, Thriller',
         why: 'Mind-bending concept with thrilling action and emotional core.',
-        image: '/incep.jpeg'
     },
     {
         id: 3,
         title: 'The Shawshank Redemption',
         genre: 'Drama',
         why: 'Powerful story about hope and friendship.',
-        image: '/TSR.jpeg'
     },
     {
         id: 4,
         title: 'The Dark Knight',
         genre: 'Action, Crime',
         why: 'Masterful villain, grounded storytelling, strong themes.',
-        image: '/dk.jpeg'
     }
 ]
